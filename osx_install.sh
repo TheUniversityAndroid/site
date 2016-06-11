dfa0329246cd90649ae9cf544ef96fa4085c9a3e
Matt Dupree <kmdupr33@gmail.com>
2016-06-11 14:46:56 -0400

    Fix install script

--- osx_install.sh
+++ osx_install.sh
@@ -1,4 +1,4 @@
 echo "export STUDIO_JDK=/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk" >> ~/.bash_profile
 
-curl http://universityandroid.com/open_studio_osx.sh > ~/Desktop/StartAndroidStudio.sh
+curl http://www.universityandroid.com/open_studio_osx.sh > ~/Desktop/StartAndroidStudio.sh
 chmod +x ~/Desktop/StartAndroidStudio.sh
